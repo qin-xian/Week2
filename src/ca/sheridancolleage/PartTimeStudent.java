02d8d99b785d2e1f841cee2d89257f4672e375b0
qin-xian <qinxian@sheridancollege.ca>
2020-01-24 11:36:08 -0500

    initial commit of source code

---
+++ src/ca/sheridancolleage/PartTimeStudent.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancolleage;
+
+/**
+ *
+ * @author Enzo
+ */
+public class PartTimeStudent extends Student{
+    private int hoursOfStudy;
+    public PartTimeStudent(String givenName, String givenID){
+            super(givenName, givenID);
+    }
+    /**
+     * @return the hoursOfStudy
+     */
+    public int getHoursOfStudy() {
+        return hoursOfStudy;
+    }
+
+    /**
+     * @param hoursOfStudy the hoursOfStudy to set
+     */
+    public void setHoursOfStudy(int hoursOfStudy) {
+        this.hoursOfStudy = hoursOfStudy;
+    }
+    
+}
